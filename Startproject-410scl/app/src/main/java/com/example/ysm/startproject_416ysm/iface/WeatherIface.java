9f9eef47767953c036cffa49a98e5467c4a3d847
PrgsQay <z1596904581@163.com>
2021-05-18 21:15:32 +0800

    1创建工程StartProject 2设计界面，获取控件 3定义接口，使用Retrofit获取天气信息

--- Startproject-410scl/app/src/main/java/com/example/ysm/startproject_416ysm/iface/WeatherIface.java
+++
@@ -1,9 +0,0 @@
-package com.example.ysm.startproject_416ysm.iface;
-
-/**
- * Created by ysm on 2017/3/14.
- */
-//不同方式获取信息
-public interface WeatherIface {
-    void getweather (String cityno,WeatherListner listner);
-}
